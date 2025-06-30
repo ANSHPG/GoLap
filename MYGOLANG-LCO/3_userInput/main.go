9addd5e7d6b487a9b77fcacc7c69195960c0406f
Anshuman Pattnaik <helloanshu04@gmail.com>
2025-07-01 01:45:30 +0530

    initial input lap 2.O

---
+++ MYGOLANG-LCO/3_userInput/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "welcome to user input"
+	fmt.Println(welcome)
+
+	//  go to pkg.go.dev to search docu. for pkg(s)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the time for lap 1")
+
+	// comma ok || err err
+
+	input, _ := reader.ReadString('\n')
+	fmt.Printf("Lap time: %d", input)
+	fmt.Printf("type: %T", input)
+}
